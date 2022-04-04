6c1e62d77bf7a952338e1686963bd1f5b4eb6a60
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-04-04 14:59:10 -0500

    added notes

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+
+
+
+/*
+ What is LINQ?
+Stands for: Language Inegrated Query
+
+Gives you the capability to query objects
+
+You can query:
+1. Objects in memory, eg collections (LINQ to Objects)
+2. Databases (LINQ to Entities)
+3. XML (LINQ to XML)
+4. ADO.NET Data Sets (LINQ to Data Sets)
+
+
+ */
