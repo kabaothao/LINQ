7c04ce5091ffc9cbb4fb36eebcbef682d922c8b7
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-04-05 16:05:41 -0500

    added codes and notes

---
+++ LINQ/BookRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    public class BookRepository
+    {
+       public IEnumerable<Book> GetBooks()
+        {
+            return new List<Book> // a generic list of books
+            {
+                new Book() {Title = "ADO.NET Step by Step", Price = 5 }, //here adding a new look and initlizing a book
+                new Book() {Title = "ASP.NET MVC", Price = 9.99f },
+                new Book() {Title = "ASP.NET Web API", Price = 12 },
+                new Book() {Title = "C# Advanced Topics", Price = 7 },
+                new Book() {Title = "C# Advanced Topics", Price = 9 },
+
+            };
+        }
+    }
+}
